b4159b9bc0f820bffd9da288e2305e5eb51db489
wandieinnocents <wandieinnocent2@gmail.com>
2022-05-24 17:28:00 +0300

    Add users

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use  App\User;
+use  App\Models\User;
 
 class AuthController extends Controller
 {
---
+++ app/User.php
@@ -0,0 +1,20 @@
+<?php
+    namespace App;
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Contracts\Auth\Authenticatable;
+    use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+    
+    class Users extends Model implements Authenticatable
+    {
+       //
+       use AuthenticableTrait;
+       protected $fillable = ['name','email','password'];
+       protected $hidden = [
+       'password'
+       ];
+       /*
+       * Get Todo of User
+       *
+       */
+     
+    }
