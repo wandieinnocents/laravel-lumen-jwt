71bbb58bb8868f83777cfe4e5e4c9a82ae9a7bf7
wandieinnocents <wandieinnocent2@gmail.com>
2022-05-26 18:46:53 +0300

    Add tests

---
+++ tests/AppTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Tests;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class AppTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/OTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Tests;
+use Laravel\Lumen\Testing\DatabaseMigrations;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class OTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
